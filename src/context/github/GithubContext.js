5f002e75600b15e4e78262495c5201a7c8c19d33
KyleHouston84 <chariot124@gmail.com>
2022-01-02 01:48:57 -0800

    Added users search form.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -13,10 +13,10 @@ export const GithubProvider = ({ children }) => {
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
-  setLoading();
-
+  
   // Get initial users (testing purposes)
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
