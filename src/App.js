38c919459f0ef256e63547ba362d0cd1d31bbe52
KyleHouston84 <chariot124@gmail.com>
2022-01-04 23:49:16 -0800

    Added alert component for validation use feedback.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,34 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
+import Alert from './components/layout/Alert';
 import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
+            <main className='conatiner mx-auto px-3 pb-12'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <main className='conatiner mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
+            <Footer />
 
-          <Footer />
-
-        </div>
-        
-      </Router>
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
