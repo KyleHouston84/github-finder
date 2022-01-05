460f2e0fff123800ce953cee52908cf446bddff5
KyleHouston84 <chariot124@gmail.com>
2022-01-05 01:00:35 -0800

    Added repo list data to the user's profile markup

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
 
 function RepoList({ repos }) {
   return (
@@ -9,8 +10,8 @@ function RepoList({ repos }) {
         </h2>
 
         { repos.map((repo) => (
-          <h3>{repo.name}</h3>
-        ))}
+          <RepoItem key={repo.id} repo={repo} />
+        )) }
       </div>
     </div>
   )
